7474ce80b46212ca69a48c5fbc0b46d6a4530ec0
gentooza <joa.cuellar@riseup.net>
2018-11-29 13:24:37 +0100

    fixed custom squareMethod

--- methods/squareMethod/squareMethod.cpp
+++ methods/squareMethod/squareMethod.cpp
@@ -22,77 +22,75 @@ along with GSResolver.  If not, see <https://www.gnu.org/licenses/>.
 #include <limits>
 #include <string>
 
-#include "../resolvMethod.h"
+#include "../base_method.h"
 
 
-class squareMethod : public resolvMethod
+class squareMethod : public base_method
 {
 public:
   virtual int analyze(cell**& cells)
   {
 
-    setMethodName("Simple squared section analisys method");
-    setMethodDescription("We remove as possible the values already defined in other fields in same squared section");
-  //square 1
-  //cols 0,1,2
-  //rows 0,1,2
-  //square 2
-  //cols 3,4,5
-  //rows 0,1,2
-  //etc...
-  int col_min = 0, col_max = 0, row_min = 0,row_max = 0;
-  std::vector<int> definedNums;
-  std::vector<int>::iterator iterDefinedNums;
-  std::vector <int> coordinates;
-  for(int square = 1 ; square <= 9 ; square++)
-    {
-      switch(square)
-	{
-	case(1):
-	  col_min = row_min = 0;
-	  col_max = row_max = 2;
-	  break;
-	case(2):
-	  col_min = 3;
-	  col_max = 5;
-	  break;
-	case(3):
-	  col_min = 6;
-	  col_max = 8;
+    //square 1
+    //cols 0,1,2
+    //rows 0,1,2
+    //square 2
+    //cols 3,4,5
+    //rows 0,1,2
+    //etc...
+    int col_min = 0, col_max = 0, row_min = 0,row_max = 0;
+    std::vector<int> definedNums;
+    std::vector<int>::iterator iterDefinedNums;
+    std::vector <int> coordinates;
+    for(int square = 1 ; square <= 9 ; square++)
+      {
+	switch(square)
+	  {
+	  case(1):
+	    col_min = row_min = 0;
+	    col_max = row_max = 2;
+	    break;
+	  case(2):
+	    col_min = 3;
+	    col_max = 5;
+	    break;
+	  case(3):
+	    col_min = 6;
+	    col_max = 8;
 	  
-	  break;
-	case(4):
-	  col_min = 0;
-	  col_max = 2;
-	  row_min = 3;
-	  row_max = 5;
-	  break;
-	case(5):
-	  col_min = 3;
-	  col_max = 5;
-	  break;
-	case(6):
-	  col_min = 6;
-	  col_max = 8;	  
-	  break;
-	case(7):
-	  col_min = 0;
-	  col_max = 2;
-	  row_min = 6;
-	  row_max = 8;
-	  break;
-	case(8):
-	  col_min = 3;
-	  col_max = 5;
-	  break;
-	case(9):
-	  col_min = 6;
-	  col_max = 8;
-	  break;
-	default:
-	  col_min = col_max = row_min = row_max = 0;
-	  break;
-	}
+	    break;
+	  case(4):
+	    col_min = 0;
+	    col_max = 2;
+	    row_min = 3;
+	    row_max = 5;
+	    break;
+	  case(5):
+	    col_min = 3;
+	    col_max = 5;
+	    break;
+	  case(6):
+	    col_min = 6;
+	    col_max = 8;	  
+	    break;
+	  case(7):
+	    col_min = 0;
+	    col_max = 2;
+	    row_min = 6;
+	    row_max = 8;
+	    break;
+	  case(8):
+	    col_min = 3;
+	    col_max = 5;
+	    break;
+	  case(9):
+	    col_min = 6;
+	    col_max = 8;
+	    break;
+	  default:
+	    col_min = col_max = row_min = row_max = 0;
+	    break;
+	  }
       
       for(int i = 0; i < sudoku_size ; i++)
 	{	  
@@ -115,15 +113,22 @@ public:
     }
   return 0;
   };
-
+  std::string ret_name()
+  {
+    return "Simple squared section analisys method";
+  };
+  std::string ret_description()
+  {
+    return "We remove as possible the values already defined in other fields in same squared section";
+  }; 
 };
 
 // the class factories
 
-extern "C" resolvMethod* create() {
+extern "C" base_method* create() {
     return new squareMethod;
 }
 
-extern "C" void destroy(resolvMethod* p) {
+extern "C" void destroy(base_method* p) {
     delete p;
 }
